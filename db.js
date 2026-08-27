2769657ddc0ed8af1ca3c3a510ee321b71e2458e
Fabian-Burdick <Fabian-Burdick@web.de>
2026-08-27 18:15:23 +0200

    add: db.js

---
+++ db.js
@@ -0,0 +1,55 @@
+let questions = [
+  {
+    question: 'Wer hat HTML erfunden?',
+    answer_1: 'Robbie Williams',
+    answer_2: 'Lady Gaga',
+    answer_3: 'Tim Berners-Lee',
+    answer_4: 'Justin Bieber',
+    right_answer: 3,
+  },
+
+  {
+    question: 'Wann entdeckte Christoph Kolumbus Amerika',
+    answer_1: '1715',
+    answer_2: '1492',
+    answer_3: '1304',
+    answer_4: '805BC',
+    right_answer: 2,
+  },
+
+  {
+    question: 'An welchem Datum fiel die Berliner Mauer?',
+    answer_1: '1989',
+    answer_2: '1970',
+    answer_3: '2000',
+    answer_4: '1955',
+    right_answer: 1,
+  },
+
+  {
+    question: 'Wie viele Oscars gewann der Film „Titanic“?',
+    answer_1: '15',
+    answer_2: '7',
+    answer_3: '3',
+    answer_4: '11',
+    right_answer: 4,
+  },
+
+  {
+    question: 'Was ist die Hauptstadt von Indien?',
+    answer_1: 'Agra',
+    answer_2: 'Nagpur',
+    answer_3: 'Neu-Delhi',
+    answer_4: 'Bangalore',
+    right_answer: 3,
+  },
+
+  {
+    question: 'Was ist die am häufigsten gesprochene Sprache in Brasilien?',
+    answer_1: 'Spanisch',
+    answer_2: 'Englisch',
+    answer_3: 'Italienisch',
+    answer_4: 'Portugiesisch',
+    right_answer: 4,
+  },
+];
